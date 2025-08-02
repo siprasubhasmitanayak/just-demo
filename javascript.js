29c85fac60f8da4b442df2e417cee450d1bbb52f
Siprasubhasmita Nayak <nayaksipra08@gmail.com>
2025-08-02 17:41:28 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
